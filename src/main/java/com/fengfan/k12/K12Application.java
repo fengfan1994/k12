492b8fc9020fbb4fd0a55153077395b02a362741
fengfan1994 <719045202@qq.com>
2020-08-22 18:30:27 +0800

    完成练习

--- src/main/java/com/fengfan/k12/K12Application.java
+++ src/main/java/com/fengfan/k12/K12Application.java
@@ -1,9 +1,11 @@
 package com.fengfan.k12;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.fengfan.k12.dao")
 public class K12Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/fengfan/k12/controller/TimeController.java
+++ src/main/java/com/fengfan/k12/controller/TimeController.java
@@ -1,7 +1,6 @@
 package com.fengfan.k12.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +9,33 @@ import java.util.Map;
 public class TimeController {
 
     @RequestMapping(value = "/time")
-    public Map<String,String> getTime(){
-        String time = Long.toString(System.currentTimeMillis()/1000L);
-        Map<String,String> mp=new HashMap<>();
-        mp.put("time",time);
+    public Map<String, String> getTime() {
+        String time = Long.toString(System.currentTimeMillis() / 1000L);
+        Map<String, String> mp = new HashMap<>();
+        mp.put("time", time);
         return mp;
+    }
+
+    @GetMapping(value = "/name")
+    public Map<String, String> getName(String name) {
+        char[] chars = name.toCharArray();
+        Map<String, String> mp = new HashMap<>();
+        String res = "";
+        for (int i = chars.length - 1; i >= 0; i--) {
+            res += chars[i];
+        }
+        mp.put("name", res);
+        return mp;
+    }
+
+    @PostMapping(value = "/sum")
+    public Map<String,Integer> getSum(@RequestBody Integer[] arr){
+        Map<String,Integer> map=new HashMap<>();
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        map.put("sum",sum);
+        return map;
     }
 }
